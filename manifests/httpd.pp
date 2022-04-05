b82902ce3fd487655f87c0823e156d0e14c3a973
YuryNaumovich <101498583+YuryNaumovich@users.noreply.github.com>
2022-04-05 17:51:11 +0200

    Update httpd.pp

--- manifests/httpd.pp
+++ manifests/httpd.pp
@@ -9,11 +9,4 @@ node 'slave1.puppet','slave2.puppet' {
  
 include::git_clone
  
- file { "/etc/httpd/conf.d/vb_static.conf":
-  mode => "0755",
-  owner => 'www-data',
-  group => 'www-data',
-  source => 'puppet:///files/vb_static.conf',
- }
- 
 }
