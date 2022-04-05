5cf5c2befe460ac54ea1faef5ceba1717960408a
YuryNaumovich <101498583+YuryNaumovich@users.noreply.github.com>
2022-04-05 17:54:59 +0200

    Update httpd.pp

--- manifests/httpd.pp
+++ manifests/httpd.pp
@@ -7,6 +7,10 @@ node 'slave1.puppet','slave2.puppet' {
   enable => true,
  }
  
-include::git_clone
- 
+ vcsrepo { '/var/www/html':
+   ensure   => present,
+   provider => git,
+   source   => 'https://github.com/Fenikks/itacademy-devops-files',
+  }
+  
 }
