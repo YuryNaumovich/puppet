60d1a384d920a4ec32511ab99c2c0e7b6d474c03
YuryNaumovich <101498583+YuryNaumovich@users.noreply.github.com>
2022-04-05 17:40:29 +0200

    Update httpd.pp

--- manifests/httpd.pp
+++ manifests/httpd.pp
@@ -12,5 +12,13 @@ node 'slave1.puppet','slave2.puppet' {
   provider => git,
   source   => 'https://github.com/Fenikks/itacademy-devops-files',
  }
- 
+}
+
+node 'slave1.puppet' {
+ file { "/etc/httpd/conf.d/vb_static.conf":
+        mode => "0755",
+        owner => 'www-data',
+        group => 'www-data',
+        source => 'puppet:///production/files/vb_static.conf',
+    }
 }
