b82c93670aaeb3be112e3a7430066c2bbb671bbe
YuryNaumovich <101498583+YuryNaumovich@users.noreply.github.com>
2022-04-06 12:06:45 +0200

    Update httpd_listen_port.pp

--- manifests/classes/httpd_listen_port.pp
+++ manifests/classes/httpd_listen_port.pp
@@ -4,7 +4,7 @@ class httpd_listen_port
    file_line { 'Listen service':
       ensure            => present,
       path              => '/etc/httpd/conf/httpd.conf',
-      line              => 'Listen $listen_port',
+      line              => "Listen $listen_port",
       match             => '^Listen',
       match_for_absence => true,
    }
