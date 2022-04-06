53fbedb429735d690132d674b19df6b893efdcb6
YuryNaumovich <101498583+YuryNaumovich@users.noreply.github.com>
2022-04-06 11:33:08 +0200

    Update httpd.pp

--- manifests/httpd.pp
+++ manifests/httpd.pp
@@ -7,7 +7,7 @@ node 'slave1.puppet','slave2.puppet' {
   enable => true,
  }
   
-  class { 'git_clone' }
+ include git_clone,
   
   file {'/etc/httpd/conf.d/vh_static.conf':
   notify => Service["httpd"],
