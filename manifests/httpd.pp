d08336e56214a585bcd6ca5583214bb65f9b3810
YuryNaumovich <101498583+YuryNaumovich@users.noreply.github.com>
2022-04-06 11:52:13 +0200

    Update httpd.pp

--- manifests/httpd.pp
+++ manifests/httpd.pp
@@ -2,20 +2,15 @@ node 'slave1.puppet','slave2.puppet' {
 
 class { 'httpd_install_and_running': } 
 class { 'git_clone': } 
-  
+class httpd_listen_port (
+$listen_port = 8080)
+
   file {'/etc/httpd/conf.d/vh_static.conf':
   notify => Service["httpd"],
     ensure => file,
     require => Package["httpd"],
     content => template("/etc/puppetlabs/code/environments/production/files/vh_static.conf"),
   }
-  
-   file_line { 'Listen service':
-      ensure            => present,
-      path              => '/etc/httpd/conf/httpd.conf',
-      line              => 'Listen 8080',
-      match             => '^Listen',
-      match_for_absence => true,
-    }
+
   
 }
