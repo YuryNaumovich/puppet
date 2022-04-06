6c40bb1ca8fbadb69a183eb373ce6dc1d31d021c
YuryNaumovich <101498583+YuryNaumovich@users.noreply.github.com>
2022-04-06 11:50:42 +0200

    Create httpd_listen_port.pp

---
+++ manifests/classes/httpd_listen_port.pp
@@ -0,0 +1,12 @@
+class httpd_listen_port 
+($listen_port = '8080')
+{
+   file_line { 'Listen service':
+      ensure            => present,
+      path              => '/etc/httpd/conf/httpd.conf',
+      line              => 'Listen $listen_port',
+      match             => '^Listen',
+      match_for_absence => true,
+   }
+ }
+    
