498ad3155f0ab7fc02ed4f8ab31f920dcac3c2c7
YuryNaumovich <101498583+YuryNaumovich@users.noreply.github.com>
2022-04-07 09:55:58 +0200

    Update httpd.pp

--- manifests/httpd.pp
+++ manifests/httpd.pp
@@ -10,7 +10,7 @@ node 'slave2.puppet' {
 class { 'httpd_install_and_running': }
 class { 'php_install': }
 class { 'git_clone': } 
-class { 'httpd_listen_port': listen_port => '8081'}
+class { 'httpd_listen_port': listen_port => '8008'}
 class { 'virtual_host_file': vh_file => 'vh_dynamic.conf'}
-class { 'open_firewall_port': open_port => '8081'}
+class { 'open_firewall_port': open_port => '8008'}
 }
