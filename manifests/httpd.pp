2b3c78e22e41616eea6e51e32416c4d11eb9cfd8
YuryNaumovich <101498583+YuryNaumovich@users.noreply.github.com>
2022-04-07 10:44:16 +0200

    Update httpd.pp

--- manifests/httpd.pp
+++ manifests/httpd.pp
@@ -4,12 +4,6 @@ class { 'git_clone': }
 class { 'httpd_listen_port': listen_port => '8080'}
 class { 'virtual_host_file': vh_file => 'vh_static.conf'}
 class { 'open_firewall_port': open_port => '8080'}
-service { 'firewalld':
-  ensure => 'running',
-  start   => '/usr/bin/systemctl start firewalld',
-  stop    => '/usr/bin/systemctl stop firewalld',
-  restart => '/usr/bin/systemctl restart firewalld'
-}
 }
 
 node 'slave2.puppet' {
@@ -19,11 +13,5 @@ class { 'git_clone': }
 class { 'httpd_listen_port': listen_port => '8008'}
 class { 'virtual_host_file': vh_file => 'vh_dynamic.conf'}
 class { 'open_firewall_port': open_port => '8008'}
-
-exec { 'myservice-systemd-reload':
-  command     => 'systemctl restart firewalld',
-  path        => [ '/usr/bin', '/bin', '/usr/sbin' ],
-  user => 'root',
-}
 
 }
