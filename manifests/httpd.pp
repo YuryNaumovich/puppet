308f3cd946b11e1ec7bcb9a70ca33fabe30204d1
YuryNaumovich <101498583+YuryNaumovich@users.noreply.github.com>
2022-04-08 11:33:11 +0200

    Update httpd.pp

--- manifests/httpd.pp
+++ manifests/httpd.pp
@@ -26,5 +26,9 @@ file { '/root/README':
 node 'puppetserver' {
 class { 'nginx_install_and_running': }
 
+exec { 'selinux_to_permissive':
+  command     => 'setenforce 0',
+  user => 'root',
+  }
 
 }
