d42c8e6b28587d4d26eddf41fd320916c4ba3993
YuryNaumovich <101498583+YuryNaumovich@users.noreply.github.com>
2022-04-08 15:16:55 +0200

    Update httpd.pp

--- manifests/httpd.pp
+++ manifests/httpd.pp
@@ -3,6 +3,13 @@ node 'mineserver.puppet' {
 package { 'java-11-openjdk-headless' :
   ensure => installed,
  }
+ 
+ wget::fetch { "download Google's index":
+       source      => 'http://www.google.com/index.html',
+       destination => '/tmp/index.html',
+       timeout     => 0,
+       verbose     => false,
+    }
 
 }
 
