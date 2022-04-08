16c7d0d95e0d08b00591875d85c4edf0386e85d1
YuryNaumovich <101498583+YuryNaumovich@users.noreply.github.com>
2022-04-08 15:27:11 +0200

    Update httpd.pp

--- manifests/httpd.pp
+++ manifests/httpd.pp
@@ -3,7 +3,11 @@ node 'mineserver.puppet' {
 package { 'java-11-openjdk-headless' :
   ensure => installed,
  }
-
+ 
+file { '/opt/minecraft':
+    ensure => 'directory',
+  }
+  
 exec { 'get_minecraft':
   cwd     => '/opt/minecraft',
   command => '/bin/wget https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar',
